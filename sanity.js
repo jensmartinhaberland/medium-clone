e9354b9df406fe230258521ac1f53ee0a79e8cea
Jens-Martin Haberland <jensmartin.haberland@icloud.com>
2022-01-27 12:23:47 +0100

    first commit

---
+++ sanity.js
@@ -0,0 +1,18 @@
+import {
+  createClient,
+  createCurrentUserHook,
+  createImageUrlBuilder,
+} from 'next-sanity'
+
+export const config = {
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  apiVersion: '2021-03-25',
+  useCdn: process.NODE_ENV === 'production',
+}
+
+export const sanityClient = createClient(config)
+
+export const urlFor = (source) => createImageUrlBuilder(config).image(source)
+
+export const useCurrentUser = createCurrentUserHook(config)
